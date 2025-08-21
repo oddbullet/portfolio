c769fba29f9b8854628f7adca1844ee1352d50fe
Bill Yang <31521678+oddbullet@users.noreply.github.com>
2025-08-21 15:07:45 -0400

    new project
    - Added interpreter project
    - Added footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,14 @@
+export default function Footer() {
+  return (
+    <div className="footer">
+      <a
+        href="https://github.com/oddbullet/portfolio"
+        target="_blank"
+        rel="noreferrer"
+        style={{ textDecoration: "none", color: "var(--accent)" }}
+      >
+        View on Github
+      </a>
+    </div>
+  );
+}
